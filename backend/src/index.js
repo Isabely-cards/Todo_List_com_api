f748ff44945ef9ad0606cd584283104bb04f0a0d
Isabely Cardoso Figueiredo <isabelycrds@gmail.com>
2023-08-04 22:52:07 -0300

    Delete index.js

--- backend/src/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-
-const app = express()
-
-app.get('/', (request, response) => response.status(200).send('Olá mundo'))
-
-
-app.listen(3333, () => console.log('Server running or port 3333'))
