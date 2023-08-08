756c979d953dca5d74e932340ece4fafb5a94b91
Isabely Cardoso Figueiredo <112524798+isabely-prog@users.noreply.github.com>
2023-08-08 14:38:58 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 require('dotenv').config()
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3333
 
 app.listen(PORT, () => console.log(`Server running or port ${PORT}`))
