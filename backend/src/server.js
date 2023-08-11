c5c0073661aa053896212c5ef25418a4a70ae1c1
Isabely Cardoso Figueiredo <isabelycrds@gmail.com>
2023-08-10 22:18:10 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 require('dotenv').config()
 
-const PORT = process.env.PORT || 3333
+const PORT = process.env.PORT || 3331
 
 app.listen(PORT, () => console.log(`Server running or port ${PORT}`))
