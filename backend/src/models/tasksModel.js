532de386d262faf472ffcd80d0695966c53ba6c9
Isabely Cardoso Figueiredo <isabelycrds@gmail.com>
2023-08-14 19:56:02 -0300

    Update tasksModel.js

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -1,25 +1,37 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-    const [tasks] = await connection.execute('SELECT * FROM tasks');
-    return tasks;
+  const [tasks] = await connection.execute('SELECT * FROM tasks');
+  return tasks;
 };
 
 const createTask = async (task) => {
-    
-    const {title} = task;
+  const { title } = task;
+  const dateUTC = new Date(Date.now()).toUTCString();
 
-    const dateUTC = new Date(Date.now()).toUTCString()
+  const query = 'INSERT INTO tasks(title, status, created_at) VALUES (?, ?, ?)';
 
-    //const esta recebendo o cod de inserir dados 
-    const query = 'INSERT INTO tasks(title, status, created_at) VALUES(?, ?, ?)';
-    // esta inserindo valores
-    const [createTask] =await connection.execute(query, [title, 'pendente', dateUTC])
+  const [createdTask] = await connection.execute(query, [title, 'pendente', dateUTC]);
+  return {insertId: createdTask.insertId};
+};
 
-    return createTask
+const deleteTask = async (id) => {
+  const [removedTask] = await connection.execute('DELETE FROM tasks WHERE id = ?', [id]);
+  return removedTask;
+};
+
+const updateTask = async (id, task) => {
+  const { title, status } = task;
+  
+  const query = 'UPDATE tasks SET title = ?, status = ? WHERE id = ?';
+
+  const [updatedTask] = await connection.execute(query, [title, status, id]);
+  return updatedTask;
 };
 
 module.exports = {
-    getAll,
-    createTask
+  getAll,
+  createTask,
+  deleteTask,
+  updateTask,
 };
