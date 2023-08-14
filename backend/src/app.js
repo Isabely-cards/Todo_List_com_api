33469e629772844cbb31d53ef2f093d504c1b544
Isabely Cardoso Figueiredo <isabelycrds@gmail.com>
2023-08-14 19:56:13 -0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const router = require('./router')
+const express = require('express');
+const cors = require('cors');
+const router = require('./router');
 
-const app = express()
+const app = express();
 
-app.use(router)
+app.use(express.json());
+app.use(cors());
+app.use(router);
 
-module.exports = app
+module.exports = app;
