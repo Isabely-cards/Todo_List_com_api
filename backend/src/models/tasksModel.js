aa820e14335d694c5ba5f11d33c98930897a03d2
Isabely Cardoso Figueiredo <112524798+Isabely-cards@users.noreply.github.com>
2023-08-14 14:44:56 -0300

    terminando o backend

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -6,18 +6,33 @@ const getAll = async () => {
 };
 
 const createTask = async (task) => {
-    
     const {title} = task;
 
-    const dateUTC = new Date(Date.now()).toUTCString()
-
+    const dateUTC = new Date(Date.now()).toUTCString();//convertendo a hora
     //const esta recebendo o cod de inserir dados 
     const query = 'INSERT INTO tasks(title, status, created_at) VALUES(?, ?, ?)';
     // esta inserindo valores
-    const createTask =await connection.execute(query, [title, 'pendente', 'asdasdas'])
+    const [createTask] = await connection.execute(query, [title, 'pendente', dateUTC]);
 
+    return {insertId: createTask.insertId};
 };
 
+const deleteTask = async (id) => {
+    const [removedTask] = await connection.execute('DELETE FROM tasks WHERE id = ?', [id]);
+    return removedTask;
+};
+
+const updateTask = async (id, task) => {
+    const {title , status} = task;
+    const query = 'UPDATE tasks SET title = ?, status = ? WHERE id = ?';
+
+    const [updateTask] = await connection.execute(query, [title, status, id] );
+    return updateTask;
+}
+
 module.exports = {
-    getAll
+    getAll,
+    createTask,
+    deleteTask,
+    updateTask,
 };
--- backend/src/router.js
+++ backend/src/router.js
@@ -2,8 +2,18 @@ const express = require('express')
 const router = express.Router()
 
 const tasksController = require('./controllers/tasksController')
+const tasksMiddleware = require('./middlewares/tasksMiddleware')
 
-router.get('/tasks', tasksController.getll )
+router.get('/tasks', tasksController.getll );
+router.post('/tasks', tasksMiddleware.validadeTitle, tasksController.createTask );
+router.delete('/tasks/:id', tasksController.deleteTask );
+router.put('/tasks/:id',
+    tasksMiddleware.validadeTitle, 
+    tasksMiddleware.validadeStatus, 
+    tasksController.updateTask,
+
+);
+
 
 
 module.exports = router
