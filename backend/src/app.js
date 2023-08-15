b3bb746804618c5308156b9cc7568b0b80fb51ba
Isabely Cardoso Figueiredo <112524798+Isabely-cards@users.noreply.github.com>
2023-08-15 09:56:23 -0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,10 +6,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
 app.use(router);
 
-<<<<<<< HEAD
 module.exports = app;
-=======
-module.exports = app;
->>>>>>> 9208a473711cc1055da15ea65e655e7facbcf537
+
