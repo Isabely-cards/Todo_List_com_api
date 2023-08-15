e22915e8c8a417f3a7d412c3cb6a2532eb369c70
Isabely Cardoso Figueiredo <112524798+Isabely-cards@users.noreply.github.com>
2023-08-15 09:56:13 -0300

    Update router.js

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,6 +1,5 @@
 const express = require('express');
 
-<<<<<<< HEAD
 const tasksController = require('./controllers/tasksController')
 const tasksMiddleware = require('./middlewares/tasksMiddleware')
 
@@ -12,22 +11,6 @@ router.put('/tasks/:id',
     tasksMiddleware.validadeStatus, 
     tasksController.updateTask,
 
-);
-
-=======
-const router = express.Router();
-
-const tasksController = require('./controllers/tasksController');
-const tasksMiddleware = require('./middlewares/tasksMiddleware');
->>>>>>> 9208a473711cc1055da15ea65e655e7facbcf537
-
-router.get('/tasks', tasksController.getAll);
-router.post('/tasks', tasksMiddleware.validateFieldTitle, tasksController.createTask);
-router.delete('/tasks/:id', tasksController.deleteTask);
-router.put('/tasks/:id',
-  tasksMiddleware.validateFieldTitle,
-  tasksMiddleware.validateFieldStatus,
-  tasksController.updateTask,
 );
 
 module.exports = router;
