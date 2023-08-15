6e1a8662c70609a39e54a60f916d6cc50f24263d
Isabely Cardoso Figueiredo <112524798+Isabely-cards@users.noreply.github.com>
2023-08-15 11:29:25 -0300

    Update tasksMiddleware.js

--- backend/src/middlewares/tasksMiddleware.js
+++ backend/src/middlewares/tasksMiddleware.js
@@ -1,35 +1,32 @@
+const validateFieldTitle = (request, response, next) => {
+    const { body } = request;
 
-const validadeTitle = (req, res, next) => {
-    const { body } = req;
+    if (body.title === undefined) {
+        return response.status(400).json({ message: 'The field "title" is required' });
+    }
 
-    if ( body.title == undefined) {
-        return res.status(400).json({message: 'The field "title" is required'});
-    };
+    if (body.title === '') {
+        return response.status(400).json({ message: 'title cannot be empty' });
+    }
 
-    if ( body.title == '') {
-        return res.status(400).json({message: 'title cannot be empty'});
-    };
-
-    next()
-
+    next();
 };
 
-const validadeStatus = (req, res, next) => {
-    const { body } = req;
+const validateFieldStatus = (request, response, next) => {
+    const { body } = request;
 
-    if ( body.status == undefined) {
-        return res.status(400).json({message: 'The field "title" is required'});
-    };
+    if (body.status === undefined) {
+        return response.status(400).json({ message: 'The field "status" is required' });
+    }
 
-    if ( body.status == '') {
-        return res.status(400).json({message: 'title cannot be empty'});
-    };
+    if (body.status === '') {
+        return response.status(400).json({ message: 'status cannot be empty' });
+    }
 
-    next()
-
+    next();
 };
 
 module.exports = {
-    validadeTitle,
-    validadeStatus,
+    validateFieldStatus,
+    validateFieldTitle,
 };
