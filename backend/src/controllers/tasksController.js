1b4d268034fddf04a91430ad9a62a962bfb279a5
Isabely Cardoso Figueiredo <112524798+Isabely-cards@users.noreply.github.com>
2023-08-15 09:56:21 -0300

    Update tasksController.js

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -5,37 +5,6 @@ const getAll = async (_request, response) => {
   return response.status(200).json(tasks);
 };
 
-<<<<<<< HEAD
-};
-
-const createTask = async (req, res) => {
-    const createTask = await tasksModel.createTask(req.body);
-    return res.status(201).json(createTask);
-};
-
-const deleteTask = async (req, res) => {
-    const { id } = req.params
-
-    await tasksModel.deleteTask(id)
-    return res.status(204).json()
-
-}
-
-const updateTask = async (req, res) => {
-    const { id } = req.params;
-
-    await tasksModel.updateTask(id, req.body);
-    return res.status(204).json()
-
-}
-
-module.exports = {
-    getll,
-    createTask,
-    deleteTask,
-    updateTask
-}
-=======
 const createTask = async (request, response) => {
   const createdTask = await tasksModel.createTask(request.body);
   return response.status(201).json(createdTask);
@@ -61,4 +30,3 @@ module.exports = {
   deleteTask,
   updateTask,
 };
->>>>>>> 9208a473711cc1055da15ea65e655e7facbcf537
