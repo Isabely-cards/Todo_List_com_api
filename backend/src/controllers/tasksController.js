5f064f4e92ff689615f2cccf6bd8525cd280bf37
Isabely Cardoso Figueiredo <112524798+Isabely-cards@users.noreply.github.com>
2023-08-15 09:45:37 -0300

    Update tasksController.js

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -1,9 +1,11 @@
-const tasksModel = require('../models/tasksModel')
+const tasksModel = require('../models/tasksModel');
 
-const getll = async (req, res) => {
-    const [tasks] = await tasksModel.getAll()
-    return res.status(200).json(tasks)
+const getAll = async (_request, response) => {
+  const tasks = await tasksModel.getAll();
+  return response.status(200).json(tasks);
+};
 
+<<<<<<< HEAD
 };
 
 const createTask = async (req, res) => {
@@ -33,3 +35,30 @@ module.exports = {
     deleteTask,
     updateTask
 }
+=======
+const createTask = async (request, response) => {
+  const createdTask = await tasksModel.createTask(request.body);
+  return response.status(201).json(createdTask);
+};
+
+const deleteTask = async (request, response) => {
+  const { id } = request.params;
+
+  await tasksModel.deleteTask(id);
+  return response.status(204).json();
+};
+
+const updateTask = async (request, response) => {
+  const { id } = request.params;
+
+  await tasksModel.updateTask(id, request.body);
+  return response.status(204).json();
+};
+
+module.exports = {
+  getAll,
+  createTask,
+  deleteTask,
+  updateTask,
+};
+>>>>>>> 9208a473711cc1055da15ea65e655e7facbcf537
