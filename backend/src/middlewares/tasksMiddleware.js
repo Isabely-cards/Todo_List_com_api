4a77420a6a331dde2bf7734ef6704db22f8a4d0f
Isabely Cardoso Figueiredo <112524798+Isabely-cards@users.noreply.github.com>
2023-08-15 09:56:18 -0300

    Update tasksMiddleware.js

--- backend/src/middlewares/tasksMiddleware.js
+++ backend/src/middlewares/tasksMiddleware.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const validadeTitle = (req, res, next) => {
     const { body } = req;
 
@@ -33,37 +33,3 @@ module.exports = {
     validadeTitle,
     validadeStatus,
 };
-=======
-const validateFieldTitle = (request, response, next) => {
-    const { body } = request;
-
-    if (body.title === undefined) {
-        return response.status(400).json({ message: 'The field "title" is required' });
-    }
-
-    if (body.title === '') {
-        return response.status(400).json({ message: 'title cannot be empty' });
-    }
-
-    next();
-};
-
-const validateFieldStatus = (request, response, next) => {
-    const { body } = request;
-
-    if (body.status === undefined) {
-        return response.status(400).json({ message: 'The field "status" is required' });
-    }
-
-    if (body.status === '') {
-        return response.status(400).json({ message: 'status cannot be empty' });
-    }
-
-    next();
-};
-
-module.exports = {
-    validateFieldStatus,
-    validateFieldTitle,
-};
->>>>>>> 9208a473711cc1055da15ea65e655e7facbcf537
