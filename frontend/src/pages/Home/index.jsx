3ee31076c244b3864f58c756a2897aa7dcf301ef
Isabely Cardoso Figueiredo <112524798+Isabely-cards@users.noreply.github.com>
2023-09-21 09:16:22 -0300

    frontend

--- frontend/src/pages/Home/index.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./styles.module.css"
-
-export default function Home() {
-    return(
-        <section className={styles.home_container}>            
-        <p>olá, tudo bem</p>
-        </section>
-    )
-}
